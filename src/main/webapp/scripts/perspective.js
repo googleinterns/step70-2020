26aaa1c2bfa46cb7576a1fc77c0ce9d4213e6b48
Jennifer Dai <jyd@google.com>
2020-06-25 00:32:46 -0700

    add comment toxicity analysis with Perspective API

---
+++ src/main/webapp/scripts/perspective.js
@@ -0,0 +1,21 @@
+function getToxicity() {
+  API_KEY = '';
+  const commentText = document.getElementById("comment-text").value;
+  const commentObject = {
+    'comment': {'text': commentText},
+    'requestedAttributes': {'TOXICITY': {}},
+    'languages': ['en']
+  };
+  const json = JSON.stringify(commentObject);
+  const request = new Request(
+    `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${API_KEY}`,
+    {method: 'POST', body: json}
+  );
+  fetch(request)
+    .then(response => response.json())
+    .then((responseJson) => {
+      const score = responseJson.attributeScores.TOXICITY.summaryScore.value;
+      const containerDOM = document.getElementById('toxicity-container');
+      containerDOM.innerText = score.toFixed(2)*100 + '% likely to be toxic.';
+    });
+}
