ebf5fadac4daa567b690914bb77e85a616bb0011
Zoe Chen <czz@google.com>
2020-07-02 11:33:43 -0400

    Add getCaptionFromId() function
    fixUp:  remove custom constructor and remove string copying

---
+++ src/main/java/com/google/sps/servlets/Caption.java
@@ -0,0 +1,46 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Caption {
+  /*
+   * Usage: String caption = new Caption(new CaptionService()).getCaptionFromID("abcdefg");
+   * 
+   * Return null if not found.
+   */
+
+  private CaptionService captionService = new CaptionService();
+  private DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+
+  public String getCaptionFromId(String videoId) {
+    // BCP47 language tags
+    final String[] languages = {"en", "en-US", "en-GB", "en-AU", "en-IE", "en-ZA"};
+    for (String language : languages) {
+      try {
+        return parseXmlFromStream(captionService.fetchStream(videoId, language));
+      } catch (SAXException | ParserConfigurationException | IOException
+          | IllegalArgumentException e) {
+        continue;
+      }
+    }
+    return null;
+  }
+
+  private String parseXmlFromStream(InputStream inputStream)
+      throws SAXException, ParserConfigurationException, IOException, IllegalArgumentException {
+    Document doc = docBuilderFactory.newDocumentBuilder().parse(inputStream);
+    NodeList textDom = doc.getElementsByTagName("text");
+    StringBuilder caption = new StringBuilder();
+    for (int i = 0; i < textDom.getLength(); i++) {
+      caption.append(textDom.item(i).getFirstChild().getNodeValue());
+      caption.append(" ");
+    }
+    return caption.toString().trim();
+  }
+}
