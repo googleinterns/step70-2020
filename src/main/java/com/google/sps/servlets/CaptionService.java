5b4ce29a08f5d4ca45e290ed119945f40ed3c152
Zoe Chen <czz@google.com>
2020-07-02 11:38:01 -0400

    Test Caption class with CaptionService mocked
    Use field level injection
    
    Assert null for illegal failed cases

--- src/main/java/com/google/sps/servlets/CaptionService.java
+++ src/main/java/com/google/sps/servlets/CaptionService.java
@@ -2,14 +2,12 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CaptionService {
-  public InputStream fetchStream(String videoId, String language)
-      throws MalformedURLException, IOException {
+  public InputStream fetchStream(String videoId, String language) throws IOException {
     if (videoId == null) {
-      throw new MalformedURLException();
+      throw new IllegalArgumentException("Video ID must be a non-null string.");
     }
     return new URL(
         String.format("http://video.google.com/timedtext?lang=%s&v=%s", language, videoId))
