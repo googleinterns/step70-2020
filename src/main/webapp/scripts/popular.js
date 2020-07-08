fde936c8a80a7ea0f8f490de10ff6d8ece4a46d6
Jennifer Dai <jyd@google.com>
2020-07-08 14:04:14 -0700

    get top trending videos from youtube

---
+++ src/main/webapp/scripts/popular.js
@@ -0,0 +1,33 @@
+function loadPopular() {
+  loadApi(function(){
+    getPopular().then((response) => {
+      for(item of response.items) {
+        // Ready in a later PR: display the item nicely on the page
+        console.log(item);
+      }
+    });
+  });
+}
+
+function getPopular() {
+  // Ready in a later PR: let user choose country, currently defaulting to US
+  return gapi.client.youtube.videos.list(getPopularRequest('US'))
+  .then(function(response) {
+    return response.result;
+  })
+  .catch((err) => {
+    alert('Oops! We were not able to get the videos from Youtube');
+    console.log('An error occurred when making API list request:', err);
+  })
+}
+
+function getPopularRequest(regionCode) {
+  return {
+    'part': [
+      'snippet,contentDetails,statistics'
+    ],
+    'chart': 'mostPopular',
+    'maxResults': 12,
+    'regionCode': regionCode
+  };
+}
