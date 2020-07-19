9103838ca642d26af9abe56f480487661b7e3520
Zoe Chen <czz@google.com>
2020-07-19 14:46:05 -0400

    Add doc

--- src/main/webapp/scripts/controller-auth.js
+++ src/main/webapp/scripts/controller-auth.js
@@ -1,15 +1,24 @@
 import { postVideoComment } from '/scripts/post-comment.js';
-import { videoId, commentElement, failedInit, successfulApiCallback, failedApiCallback, updateSigninStatus } from '/scripts/view.js';
+import { videoId, commentElement, failedInitCallback, successfulApiCallback, failedApiCallback, updateSigninStatus } from '/scripts/view.js';
 
+/** API key provied by GCP to authenticate the client */
 const apiKey = '';
+/** client ID provied by GCP to identify the client */
 const clientId = '';
+/** discovery docs used to identify API endpoints */
 const discoveryDocs = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
+/** OAuth2 scope required to post a comment */
 const scope = 'https://www.googleapis.com/auth/youtube.force-ssl';
 
+/** Load the client from Google API oauth2.0 JS library */
 function handleClientLoad() {
   gapi.load('client:auth2', initClient);
 }
 
+/** 
+ * Initialize the client with appropiate fields.
+ * And listen for changed in the signin status.
+ */
 function initClient() {
   gapi.client.init({
     apiKey,
@@ -26,17 +35,23 @@ function initClient() {
       updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get(),
         handleAuthClick, handleSignoutClick, handleCommentClick);
     })
-    .catch(failedInit);
+    .catch(failedInitCallback);
 }
 
+/** 
+ * Handler that initiates the authorization flow with a popup.
+ * The user only needs to authorize the site once.
+ */
 function handleAuthClick(event) {
   gapi.auth2.getAuthInstance().signIn();
 }
 
+/** Handler that quietly signs the user out, but does not revoke the authorization */
 function handleSignoutClick(event) {
   gapi.auth2.getAuthInstance().signOut();
 }
 
+/** Handler that posts the comment and processes the result */
 function handleCommentClick(event) {
   postVideoComment(videoId, postCommentElement.value)
     .then(successfulApiCallback)
--- src/main/webapp/scripts/view.js
+++ src/main/webapp/scripts/view.js
@@ -1,3 +1,20 @@
+/** Button that triggers authorization/sign out flow */
+const authorizeButton = document.getElementById('authorize-button');
+
+/** Hardcoded test video Id that the comment will be posted to */
+const videoId = 'gjKpAAezWQA';
+
+/** Textbox that allows user to input comments */
+const commentElement = document.getElementById('comment-text');
+
+/** Container that holds the success/error message */
+const resultContainer = document.getElementById('result-text');
+
+/**
+ * Overwrite the innerText of a DOM element identified by its Id.
+ * @param {String} text 
+ * @param {String} containerDOM 
+ */
 function updateDom(text, containerDOM) {
     while (containerDOM.firstChild) {
         containerDOM.removeChild(containerDOM.firstChild);
@@ -5,12 +22,14 @@ function updateDom(text, containerDOM) {
     containerDOM.innerText = text;
 }
 
-const authorizeButton = document.getElementById('authorize-button');
-
-const videoId = 'gjKpAAezWQA';
-const commentElement = document.getElementById('comment-text');
-const resultContainer = document.getElementById('result-text');
-
+/**
+ * Modify the UI and link handle functions depending on the sign in status.
+ * @exports
+ * @param {Boolean} isSignedIn 
+ * @param {Function} handleAuthClick 
+ * @param {Function} handleSignoutClick 
+ * @param {Function} handleCommentClick 
+ */
 function updateSigninStatus(isSignedIn, handleAuthClick, handleSignoutClick, handleCommentClick) {
     if (isSignedIn) {
         updateDom('Sign out', authorizeButton)
@@ -28,18 +47,33 @@ function updateSigninStatus(isSignedIn, handleAuthClick, handleSignoutClick, han
     }
 }
 
-function failedInit(err) {
+/**
+ * Error handler when the Google API client fails to load.
+ * @exports
+ * @param {Error} err 
+ */
+function failedInitCallback(err) {
     console.error('init error', err)
     updateDom('Failed to initialize client', resultContainer)
 }
 
+/**
+ * Display 'successful' when the comment has been posted.
+ * @exports
+ * @param {*} data 
+ */
 function successfulApiCallback(data) {
     updateDom('successful', resultContainer);
 }
 
+/**
+ * Display the error message when the comment failed to be posted.
+ * @exports
+ * @param {Error} err
+ */
 function failedApiCallback(err) {
     console.error('execution error ', err);
     updateDom(err, resultContainer);
 }
 
-export { videoId, commentElement, resultContainer, failedInit, successfulApiCallback, failedApiCallback, updateSigninStatus };
+export { videoId, commentElement, failedInitCallback, successfulApiCallback, failedApiCallback, updateSigninStatus };
