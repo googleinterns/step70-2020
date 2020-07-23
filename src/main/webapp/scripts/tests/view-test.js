82fe45f53219e5c5ba5edea1c5ee4aaa5a5b039a
Jennifer Dai <jyd@google.com>
2020-07-23 11:33:29 -0700

    change test dom names

--- src/main/webapp/scripts/tests/view-test.js
+++ src/main/webapp/scripts/tests/view-test.js
@@ -1,9 +1,9 @@
 QUnit.test('createHeader', function(assert) {
   createHeader();
-  assert.dom('h3').hasText(TITLE_TEXT);
+  assert.dom('.navbar-brand').hasText(TITLE_TEXT);
 });
 
 QUnit.test('createMenu', function(assert) {
   createMenu();
-  assert.dom('i').exists({ count: 3});
+  assert.dom('.nav-item').exists({ count: 3});
 });
