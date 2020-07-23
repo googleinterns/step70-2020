9a286f47ddc7a7bfb4021919ab81649cc49a3a87
Zoe Chen <czz@google.com>
2020-07-23 17:12:58 -0400

    OAuth2 and post comment in the front end (#14)
    * Add view.js file to control DOM
    
    * Add auth controller
    
    Modify controler-auth to use promise
    
    * Add auth div in index.html
    
    * Add doc
    
    * Renaming commentElement
    
    * Add integration tests
    
    Remove extra html set up
    
    Remove clean up
    
    * Stub gapi.auth2 instead of directly calling view
    
    Clear result container after updating sign in status
    
    * Extract fakeGetAuthInstance()
    
    * Remove dependency on controller details
    
    * Denote json as Object

--- src/main/webapp/scripts/post-comment.js
+++ src/main/webapp/scripts/post-comment.js
@@ -1,3 +1,11 @@
+/**
+ * Post a video comment by making a POST request to YouTube Data API.
+ * @exports
+ * @param {String} videoId 
+ * @param {String} commentText 
+ * @returns {String} empty string if successful post
+ * @throws {Error} with error message if failed post
+ */
 function postVideoComment(videoId, commentText) {
     return new Promise((resolve, reject) => {
         gapi.client.request(buildRequestBody(videoId, commentText))
@@ -7,6 +15,13 @@ function postVideoComment(videoId, commentText) {
     });
 }
 
+/**
+ * Build a request body expected by the API.
+ * @private
+ * @param {String} videoId 
+ * @param {String} commentContent 
+ * @returns {Object} request body for a POST comment request by video ID
+ */
 function buildRequestBody(videoId, commentContent) {
     return {
         'path': '/youtube/v3/commentThreads?part=snippet',
