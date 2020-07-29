50e45145b79de681bcfa7101fda26964a0e5ff77
Kaye Fung <kayefung@outlook.com>
2020-07-29 15:26:12 -0400

    Add tests for VideoStorageServlet.

--- src/main/java/com/google/sps/servlets/VideoStorageServlet.java
+++ src/main/java/com/google/sps/servlets/VideoStorageServlet.java
@@ -8,6 +8,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.EntityNotFoundException;
@@ -28,16 +29,14 @@ public class VideoStorageServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ArrayList<String> videos = new ArrayList<>();
 
-    Query query = new Query("Video") {
-      Order = { { "sentiment", PropertyOrder.Types.Direction.Descending },
-                { "numSearches", PropertyOrder.Types.Direction.Descending } }
-    };
+    Query query = new Query("Video")
+        .addSort("sentiment", SortDirection.DESCENDING)
+        .addSort("numSearches", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
       Key videoKey = entity.getKey();
       String id = videoKey.getName();
-      float sentiment = (float) entity.getProperty("sentiment");
       videos.add(id);
     }
 
