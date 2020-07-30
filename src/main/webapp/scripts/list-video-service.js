be2c36d941bee805861e0969d243187791eefdae
Jennifer Dai <jyd@google.com>
2020-07-29 17:07:19 -0700

    integrate region selection and displaying videos

--- src/main/webapp/scripts/list-video-service.js
+++ src/main/webapp/scripts/list-video-service.js
@@ -1,6 +1,6 @@
 function getTrendingFromYoutubeApi() {
   // Ready in a later PR: let user choose country, currently defaulting to US
-  return gapi.client.youtube.videos.list(getPopularRequest('US'))
+  return gapi.client.youtube.videos.list(getPopularRequest(getDomValue('region-select')))
   .then((response) => {
     return response.result;
   })
--- src/main/webapp/scripts/popular.js
+++ src/main/webapp/scripts/popular.js
@@ -2,7 +2,7 @@ import { getTrendingFromYoutubeApi } from './list-video-service.js';
 import { getRegions } from './region-selector.js';
 
 function loadPopular() {
-  return loadApi().then(popular.getTrendingFromYoutubeApi)
+  return popular.getTrendingFromYoutubeApi()
   .then((response) => {
     for(const item of response.items) {
       // Ready in a later PR: display the item nicely on the page
@@ -23,7 +23,7 @@ function loadPopular() {
  * @return {Promise}
  */
 function addRegionOptions() {
-  return loadApi().then(popular.getRegions)
+  return popular.getRegions()
   .then((response) => {
     for(const region of response.items) {
       addOptionToSelectList(region.snippet.gl, region.snippet.name, 'region-select');
@@ -32,7 +32,88 @@ function addRegionOptions() {
 }
 
 let popular = { loadPopular, getTrendingFromYoutubeApi, addRegionOptions, getRegions };
-popular.addRegionOptions();
-popular.loadPopular();
+loadApi().then(() => {
+  popular.addRegionOptions();
+  popular.loadPopular();
+});
+
+const regionSelectorElement = document.getElementById('region-select');
+regionSelectorElement.onchange = function(){
+  //clear page to display new region's videos
+  const popularListElement = document.getElementById('popular-list-container');
+  while(popularListElement.hasChildNodes()){
+    popularListElement.removeChild(popularListElement.firstChild);
+  }
+  popular.loadPopular();
+}
 
 export default popular;
+
+/*
+loadApi(function(){
+  getCountries().then((response) => {
+  for(const num of response.items) {
+    makeCountrySelect(num.snippet);
+  }
+});
+});
+
+function loadPopular() {
+  const listDom = document.getElementById('popular-list-container');
+  listDom.innerHTML = "";
+  loadApi(function(){
+    getPopular().then((response) => {
+      const recommended = new RecommendedVideoDisplay(listDom);
+      for(item of response.items) {
+        const video = new Video(item);
+        recommended.addVideoCard(video.createVideoCard(null));
+      }
+    });
+  });
+}
+
+function getPopular() {
+  // Ready in a later PR: let user choose country, currently defaulting to US
+  const country = document.getElementById('country').value;
+  return gapi.client.youtube.videos.list(getPopularRequest(country))
+  .then(function(response) {
+    return response.result;
+  })
+  .catch((err) => {
+    alert('Oops! We were not able to get the videos from Youtube');
+    console.log('An error occurred when making API list request:', err);
+  })
+}
+
+function getCountries() {
+  return gapi.client.youtube.i18nRegions.list({
+    "part": [
+      "snippet"
+    ],
+    "hl": "es_MX"
+  })
+  .then(function(response) {
+    return response.result;
+  });
+}
+
+function getPopularRequest(regionCode) {
+  return {
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "chart": "mostPopular",
+      'maxResults': 12,
+      "regionCode": regionCode
+  };
+}
+
+function makeCountrySelect(snippet){
+  const countrySelectDOM = document.getElementById('country');
+  const option = document.createElement('option');
+  option.value = snippet.gl;
+  option.innerText = snippet.name;
+  countrySelectDOM.appendChild(option);
+}
+
+*/
